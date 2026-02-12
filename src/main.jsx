eaf0edf8ce2c118af66bb1e51c4ab1a90cfcaff9
bamidele <devbammy@gmail.com>
2026-02-12 16:10:21 +0100

    added version analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import ReactDOM from 'react-dom/client';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 import { ThemeProvider } from './context/ThemeContext.jsx';
 import './index.scss';
 import App from './App.jsx';
 import { Toaster } from 'sonner';
 
+injectSpeedInsights();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider>
     <App />
     <Toaster position="top-center" />
-  </ThemeProvider>
+  </ThemeProvider>,
 );
