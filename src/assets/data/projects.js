fdc30cc14b8e25aa343b23ae048267ea72a695f0
Bamidele <devbammy@gmail.com>
2026-06-13 04:29:40 +0100

    before master input

---
+++ src/assets/data/projects.js
@@ -0,0 +1,90 @@
+import Dashboard from '../../assets/dashboard.png';
+import Furniture from '../../assets/furniture.png';
+import Carelynk from '../../assets/carelynk.png';
+import Rydexo from '../../assets/rydexo.png';
+import RentXSpace from '../../assets/rentxspace.png';
+import SwapAm from '../../assets/swapam.png';
+
+import CareerMan from '../../assets/career.png';
+import QrGenerator from '../../assets/eleventh.png';
+import EmeyoShop from '../../assets/emayoShop.png';
+import CoachSeyifunmi from '../../assets/coach.png';
+import EmayoPortfolio from '../../assets/emayo.png';
+
+export const topRowWorks = [
+  {
+    id: 't1',
+    name: 'Staffium',
+    img: Dashboard,
+    href: 'https://stafflo.vercel.app/',
+  },
+  {
+    id: 't2',
+    name: 'Furniture eCommerce',
+    img: Furniture,
+    href: '/work/furniture',
+  },
+  {
+    id: 't3',
+    name: 'Carelynk',
+    img: Carelynk,
+    href: '/work/carelynk',
+  },
+  {
+    id: 't4',
+    name: 'Rydexo',
+    img: Rydexo,
+    href: '/work/rydexo',
+  },
+  {
+    id: 't5',
+    name: 'RentXSpace',
+    img: RentXSpace,
+    href: '/work/rentxspace',
+  },
+  {
+    id: 't6',
+    name: 'SwapAm',
+    img: SwapAm,
+    href: '/work/swapam',
+  },
+];
+
+export const bottomRowWorks = [
+  {
+    id: 'b1',
+    name: 'CareerMan',
+    img: CareerMan,
+    href: '/work/careerman',
+  },
+  {
+    id: 'b2',
+    name: 'Event QR Code Generator',
+    img: QrGenerator,
+    href: '/work/qr-generator',
+  },
+  {
+    id: 'b3',
+    name: 'Emeyo Trend Wears',
+    img: EmeyoShop,
+    href: '/work/emeyo-shop',
+  },
+  {
+    id: 'b4',
+    name: 'Coach Seyifunmi',
+    img: CoachSeyifunmi,
+    href: '/work/coach-seyifunmi',
+  },
+  {
+    id: 'b5',
+    name: 'Emayo Portfolio',
+    img: EmayoPortfolio,
+    href: '/work/emayo-portfolio',
+  },
+  {
+    id: 'b6',
+    name: 'RentXSpace',
+    img: RentXSpace,
+    href: '/work/rentxspace',
+  },
+];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from '../../components/home/Hero';
+import Works from '../../components/home/Works';
 import styles from './home.module.scss';
 
 const Home = () => {
   return (
     <div className={styles.home}>
       <Hero />
+      <Works />
     </div>
   );
 };
